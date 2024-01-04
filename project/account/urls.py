b0a8713d3433811b9d8fcd80871cddd417f20a7a
yacine47 <iyac2077@gmail.com>
2024-01-04 08:50:29 +0100

    add project

---
+++ project/account/urls.py
@@ -0,0 +1,11 @@
+
+
+
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('register/',views.register,name='register'),
+    path('userinfo/',views.current_user,name='user_info'),
+    path('userinfo/profile/',views.current_profile,name='current_profile'),
+    path('userinfo/update',views.update_user,name='update_user')
+]
